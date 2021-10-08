2b9b8d87a917b8a88ab6b7ea261e1fa6f33a9d4e
nibedita260 <deepi.nibedita260.com>
2021-10-08 22:25:32 +0530

    [Nibedita] Add: FirstNameValidation Regular Expression Features Implemented

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RegularExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isExit = false;
+            int options;
+            while (!isExit)
+            {
+                Console.WriteLine("Choose 1.FirstNameValidate");
+                options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:RegularExpression regularExpression = new RegularExpression();
+                        Console.WriteLine("enter firstname");
+                        string firstName = Console.ReadLine();
+                        regularExpression.FirstnameCheck(firstName);
+                        break;
+                    default:
+                        Console.WriteLine("choose valid oprions");
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ RegularExpression/RegularExpression.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegularExpression
+{
+    class RegularExpression
+    {
+        const string FIRSTNAME_PATTERN = "^[A-Z]{1}[a-z]{3,20}$";
+        public void FirstnameCheck(string firstName)
+        {
+            if (Regex.IsMatch(firstName, FIRSTNAME_PATTERN)){
+                Console.WriteLine("First Name Validated");
+            }
+            else
+            {
+                Console.WriteLine("First Name is not Validated");
+            }
+        }
+    }
+}
